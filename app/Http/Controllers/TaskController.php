0e0e862422c2c449a008da0932bb26ec3e14345b
komoore1 <kmkomoore@gmail.com>
2016-06-07 19:25:40 +0630

    Until Displayint te tasks

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class TaskController extends Controller
+{	
+	/**
+     * The task repository instance.
+     *
+     * @var TaskRepository
+     */
+    protected $tasks;
+	
+  /**
+     * Create a new controller instance.
+     *
+     * @param  TaskRepository  $tasks
+     * @return void
+     */
+    public function __construct(TaskRepository $tasks)
+    {
+        $this->middleware('auth');
+
+        $this->tasks = $tasks;
+    }
+	
+	/**
+     * Display a list of all of the user's task.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        return view('tasks.index', [
+            'tasks' => $this->tasks->forUser($request->user()),
+        ]);
+    }
+	
+	/**
+	 * Create a new task.
+	 *
+	 * @param  Request  $request
+	 * @return Response
+	 */
+	public function store(Request $request)
+	{
+		$this->validate($request, [
+			'name' => 'required|max:255',
+		]);
+
+		$request->user()->tasks()->create([
+        'name' => $request->name,
+    ]);
+
+    return redirect('/tasks');
+	}
+}
